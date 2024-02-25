369aa98330872bad75b64e18ac28be8804444004
Gaurav Kulkarni <gykulkarni23@gmail.com>
2024-02-25 21:56:12 +0530

    Backend data Display and Filtering

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,27 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
+  let options =  props.options;
+  let priceOptions = Object.keys(options);
+
   return (
     <div>
-      <div className="card mt-3" style={{ width: "18rem", maxHeight: "460px", objectFit: "contain !important" }}>
-        <img  src="https://source.unsplash.com/random/9€px700/?burger" className="card-img-top" alt="..." />
+<div className="card mt-3" style={{ width: "16rem", maxHeight: "360px" }}>
+        <img src={props.ImgSrc} className="card-img-top" alt="..." style={{ height: "220px", objectFit: "fill" }} />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text"> Some quick example text</p>
-          <div className="container w-100">
-            <select className="m-2 h-100 w-40 bg-success rounded">
+          <h5 className="card-title">{props.foodName}</h5>
+
+          <div className='container w-100 p-0' style={{ height: "38px" }}>
+            <select className="m-2 h-100 w-20 bg-success text-black rounded" style={{ select: "#FF0000" }}>
               {Array.from(Array(6), (e, i) => {
                 return (
-                  <option key={i + 1} value={1 + 1}>
-                    {i + 1}
-                  </option>
-                );
+                  <option key={i + 1} value={i + 1}>{i + 1}</option>)
               })}
             </select>
-            <select className="card-text m-2 h-100 bg-success rounded">
-              <option value="half"> Half</option>
-              <option value="full">Full</option>
+            <select className="card-text m-2 w-50 h-100 bg-success rounded">
+              {priceOptions.map((data)=>{
+                return <option key={data} value={data}> {data} </option>
+              })}
               <div className="d-inline h-100 fs-5">Total Price</div>
             </select>
           </div>
