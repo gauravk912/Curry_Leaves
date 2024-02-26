1e132d77b6057c07c9214880a9d64c185ebbfbb3
Gaurav Kulkarni <gykulkarni23@gmail.com>
2024-02-26 21:06:27 +0530

    Carousal Edits

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
-import Carousal from "../components/Carousal";
+// import Carousal from "../components/Carousal";
 
 export default function () {
   const [foodCat, setFoodCat] = useState([]);
