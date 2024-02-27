846bc5ddaa2b9b13791ae6792c113d6722949a3c
Gaurav Kulkarni <gykulkarni23@gmail.com>
2024-02-27 23:09:59 +0530

    Context

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,57 @@
 import React from "react";
 
 export default function Card(props) {
-  let options =  props.options;
+  let options = props.options;
   let priceOptions = Object.keys(options);
+
+  const handleAddToCart= () =>{
+    console.log("test");
+  }
 
   return (
     <div>
-<div className="card mt-3" style={{ width: "16rem", maxHeight: "360px" }}>
-        <img src={props.ImgSrc} className="card-img-top" alt="..." style={{ height: "220px", objectFit: "fill" }} />
+      <div className="card mt-3" style={{ width: "16rem", maxHeight: "360px" }}>
+        <img
+          src={props.ImgSrc}
+          className="card-img-top"
+          alt="..."
+          style={{ height: "180px", objectFit: "fill" }}
+        />
         <div className="card-body">
           <h5 className="card-title">{props.foodName}</h5>
 
-          <div className='container w-100 p-0' style={{ height: "38px" }}>
-            <select className="m-2 h-100 w-20 bg-success text-black rounded" style={{ select: "#FF0000" }}>
+          <div className="container w-100 p-0" style={{ height: "40px" }}>
+            <select
+              className="m-2 h-100 w-20 bg-success text-black rounded"
+              style={{ select: "#FF0000" }}
+            >
               {Array.from(Array(6), (e, i) => {
                 return (
-                  <option key={i + 1} value={i + 1}>{i + 1}</option>)
+                  <option key={i + 1} value={i + 1}>
+                    {i + 1}
+                  </option>
+                );
               })}
             </select>
             <select className="card-text m-2 w-50 h-100 bg-success rounded">
-              {priceOptions.map((data)=>{
-                return <option key={data} value={data}> {data} </option>
+              {priceOptions.map((data) => {
+                return (
+                  <option key={data} value={data}>
+                    {" "}
+                    {data}{" "}
+                  </option>
+                );
               })}
-              <div className="d-inline h-100 fs-5">Total Price</div>
+              <div className="d-inline fs-5">Total Price</div>
             </select>
           </div>
+          <hr></hr>
+          <button
+            className={`btn btn-success justify-center ms-2`}
+            onClick={handleAddToCart}
+          >
+            Add To Cart
+          </button>
         </div>
       </div>
     </div>
---
+++ src/components/ContextReducer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const CartStateContext = createContext();
+const CartDispatchContext = createContext();
+
+export const CartProvider = ({}) => {
+    
+}
